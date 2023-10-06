dde29456d0006f690fa226126cb84cd5276b4b45
Ankit Sharma <ankitsharma290101@gmail.com>
2023-10-06 23:12:56 +0530

    sidebar-top

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,32 @@
+import { Avatar } from "@mui/material";
+import React from "react";
+import "./sidebar.css"
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="sidebar_top">
+        <img src="https://media.licdn.com/dms/image/D4D16AQFkF6U-fTBt7Q/profile-displaybackgroundimage-shrink_350_1400/0/1695575160006?e=1701907200&v=beta&t=GJy-aI7eAN0-rzqaamXx1ijFLf8hEubXrzCQnkKIiaE" alt="bg-img" />
+        <Avatar className="sidebar_avatar" />
+        <h2>Ankit Sharma</h2>
+        <h4>Ankitsharma290101@gmail.com</h4>
+      </div>
+
+      <div className="sidebar_stats">
+        <div className="sidebar_stat">
+          <p>Who Viewed You</p>
+          <p className="sidebar_statsNumber">2,500</p>
+        </div>
+        <div className="sidebar_stat">
+          <p >Post impressions</p>
+          <p className="sidebar_statsNumber">3,521</p>
+        </div>
+      </div>
+
+      <div className="sidebar_bottom"></div>
+      <p>Recent</p>
+    </div>
+  );
+};
+
+export default Sidebar;
